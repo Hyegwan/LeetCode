010c7cef246194d2c0dd9bb3492a80870d18e9f3
Hyegwan <86900769+Hyegwan@users.noreply.github.com>
2023-10-23 16:11:17 +0900

    Time: 1583 ms (7.3%), Space: 0B (100%) - LeetHub

---
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -0,0 +1,36 @@
+select round(sum(tiv_2016), 2) as tiv_2016
+from insurance
+where tiv_2015 IN (
+    select tiv_2015
+    from insurance
+    group by tiv_2015
+    having count(*) > 1)
+and (lat, lon) NOT IN (
+    select lat, lon
+    from insurance
+    group by lat, lon
+    having count(*) > 1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# select round(sum(tiv_2016), 2) as tiv_2016
+# from insurance i1
+# where tiv_2015 in (
+#     select tiv_2015
+#     from insurance i2
+#     where i2.pid != i1.pid)
+# and (lat, lon) not in (
+#     select lat, lon
+#     from insurance i3
+#     where i3.pid != i1.pid)
