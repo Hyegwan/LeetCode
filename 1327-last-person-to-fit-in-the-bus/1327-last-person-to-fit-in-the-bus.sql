b04b8a02c79ac9c1af4052e4e8a74806c044376d
Hyegwan <86900769+Hyegwan@users.noreply.github.com>
2023-10-24 22:45:53 +0900

    Time: 4230 ms (5%), Space: 0B (100%) - LeetHub

--- 1327-last-person-to-fit-in-the-bus/1327-last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/1327-last-person-to-fit-in-the-bus.sql
@@ -1,13 +1,22 @@
-with tb as (
-    select *
-        , (select sum(weight)
-            from queue q2
-            where q2.turn <= q1.turn) as cum_weight
-    from queue q1
-    order by turn)
-
-select person_name
-from tb
-where cum_weight <= 1000
-order by cum_weight desc
+select q1.person_name
+from queue q1 
+     join queue q2 on q1.turn >= q2.turn
+group by q1.turn, q1.person_name
+having sum(q2.weight) <= 1000
+order by q1.turn desc
 limit 1
+
+
+# with tb as (
+#     select *
+#         , (select sum(weight)
+#             from queue q2
+#             where q2.turn <= q1.turn) as cum_weight
+#     from queue q1
+#     order by turn)
+
+# select person_name
+# from tb
+# where cum_weight <= 1000
+# order by cum_weight desc
+# limit 1
